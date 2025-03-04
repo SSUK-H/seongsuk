4bf7f7e5ceada04c65bacf84ad89bae88f517148
Seongsuk Hong <ssukh77@naver.com>
2025-03-04 19:16:32 +0900

    ✨Feat: CLI 설정 및 예외 처리 추가
    - CLI 명령어 만들기 위한 commander 설치
    - CLI 이름, 설명, 버전 설정 (package.json에서 description 수정)
    - 정의되지 않은 명령어 입력 시 오류 메시지 출력 후 도움말 실행

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import figlet from "figlet";
 import chalk from "chalk";
+import { Command } from "commander";
 import { CLI_INFO, MESSAGES } from "./utils/constants";
+
+const program = new Command();
 
 // 텍스트 배너
 console.log(
@@ -15,3 +18,20 @@ console.log(
   ),
 );
 console.log(chalk.blue(MESSAGES.welcome));
+
+// CLI 정보
+program
+  .name(CLI_INFO.name)
+  .description(chalk.yellow(CLI_INFO.description))
+  .version(CLI_INFO.version, "-v, --version");
+
+// 정의되지 않은 명령어인 경우 자동으로 help 실행
+program.on("command:*", (operands) => {
+  console.error(
+    chalk.red(`${MESSAGES.error_unknown_command} '${operands[0]}'`),
+  );
+  program.help();
+  process.exit(1);
+});
+
+program.parse(process.argv);
