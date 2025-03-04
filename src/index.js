ca785846b2346346297ef26817e0f8f43b6fbcd6
Seongsuk Hong <ssukh77@naver.com>
2025-03-04 16:21:45 +0900

    💄Design: 환영 문구 및 스타일링 추가
    - 스타일링을 위한 chalk 설치 (최신 버전은 ESM 지원으로 4.1.2 버전 설치)
    - CLI 정보 package.json에서 가져오도록 수정
    - 환영 문구 및 스타일링 적용

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import figlet from "figlet";
+import chalk from "chalk";
+import { CLI_INFO, MESSAGES } from "./utils/constants";
 
 // 텍스트 배너
 console.log(
-  figlet.textSync("SEONGSUK", {
-    font: "Dancing Font", // Ghost , Standard , Graffiti , Dancing Font , Slant , Pagga
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 80,
-    whitespaceBreak: true,
-  }),
+  chalk.blue(
+    figlet.textSync(CLI_INFO.name.toLowerCase(), {
+      font: "Dancing Font", // Ghost , Standard , Graffiti , Dancing Font , Slant , Pagga
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    }),
+  ),
 );
+console.log(chalk.blue(MESSAGES.welcome));
---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+import pkg from "../../package.json";
+
+// CLI 정보
+export const CLI_INFO = {
+  name: pkg.name,
+  version: pkg.version,
+  description: pkg.description,
+};
+
+// 기본 메세지
+export const MESSAGES = {
+  welcome: `
+========================================================
+  
+  🚀 Welcome to Seongsuk's CLI! 🚀
+
+  Nice to meet you:)
+  I am Seongsuk, a developer who codes even in dreams.
+
+  Use the commands below to learn more about me!
+
+========================================================
+`,
+};
